d34b4232af2300e59fbcdcfd7a2a4b4ea5ec0ffd
thegauravjha <gaurav.jhaji20@gmail.com>
2024-04-18 20:08:26 +0530

    Added Navbar and Button

---
+++ src/Components/FormList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormList = () => {
+
+    return (
+        <div className='container'>
+            <div className='btn'>
+                <button className='create-btn'>Create New From</button>
+            </div>
+        </div>
+    )
+}
+
+export default FormList
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <h1>
+                Form Craft
+            </h1>
+            <ul className='menu'>
+                <li className='menu-item'>About Me</li>
+                <li className='menu-item'>LinkedIn</li>
+                <li className='menu-item'>Github Link</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
